1e755d2516cf707465346935b9e018b21a35bc42
IvanLink-py <ostaninivan8@gmail.com>
2025-04-18 21:10:30 +0500

    Инициализация окна

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
+# -*- coding: utf-8 -*-
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+################################################################################
+## Form generated from reading UI file 'designerpaTrbe.ui'
+##
+## Created by: Qt User Interface Compiler version 6.9.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QMainWindow, QMenuBar, QSizePolicy,
+    QStatusBar, QWidget)
+
+class Ui_MainWindow(QMainWindow):
+    def setupUi(self):
+        MainWindow = self
+        MainWindow.resize(800, 600)
+        self.centralwidget = QWidget(MainWindow)
+        MainWindow.setCentralWidget(self.centralwidget)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+app = QApplication(sys.argv)
+wnd = Ui_MainWindow()
+wnd.setupUi()
+wnd.show()
+sys.exit(app.exec())
